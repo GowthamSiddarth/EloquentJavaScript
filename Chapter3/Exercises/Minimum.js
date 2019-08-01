fc950f246f6ec015d83c5bfa1e99a66f044e4d34
Gowtham Behara <gothsiddu@gmail.com>
2019-08-01 19:13:04 +0530

    Define func to compute min of two values

--- Chapter3/Exercises/Minimum.js
+++ Chapter3/Exercises/Minimum.js
@@ -3,3 +3,8 @@ The previous chapter introduced the standard function Math.min that returns
 its smallest argument. We can build something like that now. Write a function
 min that takes two arguments and returns their minimum.
 */
+
+const minOfTwo = (x, y) => x < y ? x : y;
+
+let x = 5, y = "as";
+console.log(minOfTwo(x, y));
