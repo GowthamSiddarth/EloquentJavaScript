f7d0ce851693e5b67c534df3a940a3f9ea807760
Gowtham Behara <gothsiddu@gmail.com>
2020-03-06 00:29:52 +0530

    Define func to flatten array using reduce and concat with depth optional parameter

--- Chapter5/Exercises/Flattening.js
+++ Chapter5/Exercises/Flattening.js
@@ -6,5 +6,8 @@ arrays.
 
 const flattenWithReduceAndConcat = array => array.reduce((a, b) => a.concat(b), []);
 
+const flattenWithReduceAndConcatRec = (array, depth = 1) => depth > 0 ? array.reduce((acc, val) => acc.concat(Array.isArray(val) ? flattenWithReduceAndConcatRec(val, depth - 1) : val), []) : array;
+
 let array = [1, 2, [3, 4, [5, 6]]];
 console.log(flattenWithReduceAndConcat(array));
+console.log(flattenWithReduceAndConcatRec(array, 10));
