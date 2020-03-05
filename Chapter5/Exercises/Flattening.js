18287f5a1d3ee1bf3bd4bcdca31bebf7afae73d2
Gowtham Behara <gothsiddu@gmail.com>
2020-03-06 00:17:18 +0530

    Define func to flatten array using reduce and concat

--- Chapter5/Exercises/Flattening.js
+++ Chapter5/Exercises/Flattening.js
@@ -3,3 +3,8 @@ Use the reduce method in combination with the concat method to “flatten”
 an array of arrays into a single array that has all the elements of the original
 arrays.
 */
+
+const flattenWithReduceAndConcat = array => array.reduce((a, b) => a.concat(b), []);
+
+let array = [1, 2, [3, 4, [5, 6]]];
+console.log(flattenWithReduceAndConcat(array));
