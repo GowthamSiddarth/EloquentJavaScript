0d40f062781225c985427c3333e97620dae9fe78
Gowtham Behara <gothsiddu@gmail.com>
2020-03-11 12:07:01 +0530

    Define class with vector functionality

--- Chapter6/Exercises/AVectorType.js
+++ Chapter6/Exercises/AVectorType.js
@@ -8,3 +8,26 @@ of the two vectors’ (this and the parameter) x and y values.
 Add a getter property length to the prototype that computes the length of
 the vector—that is, the distance of the point (x, y) from the origin (0, 0).
 */
+
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(vector) {
+        return new Vec(this.x + vector.x, this.y + vector.y);
+    }
+
+    minus(vector) {
+        return new Vec(this.x - vector.x, this.y - vector.y);
+    }
+
+    get length() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    toString() {
+        return this.x + ", " + this.y;
+    }
+}
